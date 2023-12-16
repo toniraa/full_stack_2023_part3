9d65d71126b0ba76990a8b5c21bdaedc709ddcdb
Toni Raatikainen <toraatikainen@gmail.com>
2023-12-16 21:55:43 +0200

    Part3 ready

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const express = require('express')
+//const mongoose = require('mongoose')
+const Person = require('./models/person')
 var morgan = require('morgan')
 const cors = require('cors')
 
@@ -9,61 +12,45 @@ app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-
 
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+
+
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+  .catch(error => {
+    console.log(error)
+    response.status(400).end()
+  })
+ // response.json(persons)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
+
+
+//mongoose.set('strictQuery', false)
+//mongoose.connect(url)
 
   if (!body.name || body.name == '') {
     return response.status(400).json({ 
@@ -76,34 +63,29 @@ app.post('/api/persons', (request, response) => {
       error: 'Number is missing' 
     })
   }
-
-  const existingPerson = persons.find(person => person.name === body.name)
-
-  if(existingPerson) {
-    return response.status(400).json({ 
-      error: 'Name must be unique' 
-    })
-  }
-
-
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
-    number: body.number || false,
-  }
+    number: body.number,
+  })
 
-  persons = persons.concat(person)
+  person.save().then(result => {
+    
+    Person.find({}).then(persons => {
+      response.json(persons)
+      //mongoose.connection.close()
+    })
+  })
+  .catch(error => next(error))
 
-  response.json(persons)
 })
 
 
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/info', (request, response) => {
@@ -114,6 +96,21 @@ app.get('/api/info', (request, response) => {
 })
 
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
